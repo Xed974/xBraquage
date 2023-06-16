06aa3c63bdfe70a1e197d8851d6ce4e31382ad09
Xed <93364057+Xed974@users.noreply.github.com>
2023-06-16 20:29:21 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,16 +4,6 @@ games {'gta5'}
 shared_scripts { "shared/*.lua" }
 
 client_scripts {
-    "libs/RMenu.lua",
-    "libs/menu/RageUI.lua",
-    "libs/menu/Menu.lua",
-    "libs/menu/MenuController.lua",
-    "libs/components/*.lua",
-    "libs/menu/elements/*.lua",
-    "libs/menu/items/*.lua",
-    "libs/menu/panels/*.lua",
-    "libs/menu/windows/*.lua",
-
     "client/*.lua",
 }
 
